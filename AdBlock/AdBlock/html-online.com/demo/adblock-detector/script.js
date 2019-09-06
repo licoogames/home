ec88f004439f7adde800b5d3f7ed8cf514acfcb0
licoogames <54994627+licoogames@users.noreply.github.com>
2019-09-06 17:21:52 +0100

    Update script.js

--- AdBlock/AdBlock/html-online.com/demo/adblock-detector/script.js
+++ AdBlock/AdBlock/html-online.com/demo/adblock-detector/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     if($("#wrapfabtest").height() > 0) {
-		alert('No AdBlock :)');
 	} else {
-		alert('AdBlock Detected ');
+		alert('Welcome to Licoo Games! To keep the site up and running, please take a minute to disable your AdBlocker. We promise not to fill the whole of the website up will advertisements. If you wish to continue using Licoo Games with your AdBlocker on, feel free. If you enjoy playing our games, please disable your AdBlocker and keep the site up and running! Thank you for reading this message, I hope you decide to disable the AdBlocker and either way, enjoy the games!');
 	}
 });
